8f7767320b9fe4da73e28e420bc06b96c5265c94
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2022-03-28 00:10:51 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,37 +3,12 @@ import pytest
 from sys import os
 
 
+def underweight():
+  assert main.classifyBMI(18.4)=="underweight"
+def normal():
+  assert main.classifyBMI(18.6)=="underweight"
+def overweight():
+  assert main.classifyBMI(25.1)=="underweight"
+def obese():
+  assert main.classifyBMI(31.0)=="underweight"
 
-# TEST(BMITest, Greaterthan0Lessthan100) {
-# 	float bmi = calculateBMI(56, 119);
-
-# 	EXPECT_TRUE(bmi > 0.0 && bmi < 100.0);
-
-
-# }
-
-# TEST(BMITest, classifyBMIUnderweight) {
-
-# 	string classify = classifyBMI(18.4);
-
-# 	EXPECT_EQ(classify, "underweight");
-# }
-
-# TEST(BMITest, classifyBMINoraml) {
-# 	string classify = classifyBMI(18.6);
-
-# 	EXPECT_EQ(classify, "normal");
-
-# }
-# TEST(BMITest, classifyBMIOverweight) {
-# 	string classify = classifyBMI(25.1);
-
-# 	EXPECT_EQ(classify, "overweight");
-
-# }
-# TEST(BMITest, classifyBMIObese) {
-# 	string classify = classifyBMI(31.0);
-
-# 	EXPECT_EQ(classify, "obese");
-
-# }
