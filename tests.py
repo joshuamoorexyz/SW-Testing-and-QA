30ed2d69259eea353cbb08cc2b801bf76e8a32c7
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2022-03-28 00:03:46 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,39 @@
+
+import pytest
+from sys import os
+
+
+
+# TEST(BMITest, Greaterthan0Lessthan100) {
+# 	float bmi = calculateBMI(56, 119);
+
+# 	EXPECT_TRUE(bmi > 0.0 && bmi < 100.0);
+
+
+# }
+
+# TEST(BMITest, classifyBMIUnderweight) {
+
+# 	string classify = classifyBMI(18.4);
+
+# 	EXPECT_EQ(classify, "underweight");
+# }
+
+# TEST(BMITest, classifyBMINoraml) {
+# 	string classify = classifyBMI(18.6);
+
+# 	EXPECT_EQ(classify, "normal");
+
+# }
+# TEST(BMITest, classifyBMIOverweight) {
+# 	string classify = classifyBMI(25.1);
+
+# 	EXPECT_EQ(classify, "overweight");
+
+# }
+# TEST(BMITest, classifyBMIObese) {
+# 	string classify = classifyBMI(31.0);
+
+# 	EXPECT_EQ(classify, "obese");
+
+# }
