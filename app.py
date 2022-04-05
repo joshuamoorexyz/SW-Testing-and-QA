e606307d5da4139f0c506cee28f961f37e365c0b
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2022-04-04 23:13:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,8 @@ def main():
 
 
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
