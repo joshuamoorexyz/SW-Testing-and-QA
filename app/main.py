6328c0e5a2298c133df5dc96d817ddbc770b0ff5
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2022-04-04 22:34:20 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@
 from sre_constants import FAILURE, SUCCESS
 from functions import *
 from flask import Flask, render_template, request
-import os 
 
-port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
 
@@ -24,8 +22,6 @@ def main():
 
 
 
-if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=port, debug=True)
 
 
 
