f0c088d95ea8c763b849df2af4531ea6d6c15456
joshua moore <joshuamoore@posteo.net>
2022-04-07 19:17:02 -0500

    fixed css styling and rounded BMI for user

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def main():
         bmi = float(calculateBMI(height,weight))
         bmiclassification=classifyBMI(bmi)
     return render_template("index.html",
-	                        bmi=bmi,bmiclassification=bmiclassification)
+	                        bmi=round(bmi),bmiclassification=bmiclassification)
 
 
 
